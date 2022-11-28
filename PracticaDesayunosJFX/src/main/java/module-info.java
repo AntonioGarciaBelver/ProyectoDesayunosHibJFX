2459d5fc0722cfc31cad215fe865f03e9946fe91
AntonioGarciaBelver <93923363+AntonioGarciaBelver@users.noreply.github.com>
2022-11-28 12:28:37 +0100

    Proyecto Desayunos Hibernate y JavaFX

---
+++ PracticaDesayunosJFX/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.mycompany.practicadesayunosjfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.persistence;
+    requires lombok;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+    requires java.sql;
+    requires java.base;
+
+    opens models;
+    opens com.mycompany.practicadesayunosjfx to javafx.fxml;
+    exports com.mycompany.practicadesayunosjfx;
+}
+
